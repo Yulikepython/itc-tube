7a92378c5f4f03138bd9e46cf965cc45428dd94b
Yu <admin@itc.tokyo>
2022-12-08 14:47:23 +0900

    チュートリアル1-4：モデルクラスの作成方法

--- src/nippo/models.py
+++ src/nippo/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class NippoModel(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+    timestamp = models.DateTimeField(auto_now_add=True)
