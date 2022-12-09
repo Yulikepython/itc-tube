00bddd10685c88d38ab0b9c52938e758a992a146
Yu <admin@itc.tokyo>
2022-12-09 12:48:01 +0900

    チュートリアル1-6、1-7：管理画面からのデータ登録とstrメソッドについて

--- src/nippo/models.py
+++ src/nippo/models.py
@@ -4,3 +4,6 @@ class NippoModel(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=1000)
     timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
