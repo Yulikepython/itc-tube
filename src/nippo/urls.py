7884eb1f367ac6f9e47fd1c9da83b085ae0a49d0
Yu <admin@itc.tokyo>
2022-12-09 14:25:56 +0900

    チュートリアル2-2：views.pyからHTMLテンプレートへ値を渡す

--- src/nippo/urls.py
+++ src/nippo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import nippoListView
+from .views import nippoListView, nippoDetailView
  
 urlpatterns = [
   path("", nippoListView),
+  path("detail/",nippoDetailView),
 ]
--- src/nippo/views.py
+++ src/nippo/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from random import randint
 
 def nippoListView(request):
 	return render(request, "nippo/nippo-list.html")
+
+def nippoDetailView(request):
+    template_name="nippo/nippo-detail.html"
+    random_int = randint(1,10)
+    ctx = {
+        "random_number": random_int,
+    }
+    return render(request, template_name, ctx)
