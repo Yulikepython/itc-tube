28c995a3de3dfa231667eec8b042c9d27dbc781c
Yu <admin@itc.tokyo>
2022-12-09 13:14:57 +0900

    チュートリアル2-1：HTMLテンプレートを表示する方法

--- src/nippo/urls.py
+++ src/nippo/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import nippoListView
  
 urlpatterns = [
-  
+  path("", nippoListView),
 ]
--- src/nippo/views.py
+++ src/nippo/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def nippoListView(request):
+	return render(request, "nippo/nippo-list.html")
