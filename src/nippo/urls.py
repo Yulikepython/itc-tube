90785d2bdbbe37825478ed11c3b2e07151fabc7f
Yu <admin@itc.tokyo>
2022-12-13 12:56:10 +0900

    チュートリアル2-6：ページのリンク先をページ名で指定する

--- src/nippo/urls.py
+++ src/nippo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import nippoListView, nippoDetailView,nippoCreateView
  
 urlpatterns = [
-  path("", nippoListView),
-  path("detail/<int:number>/",nippoDetailView),
-  path("create/",nippoCreateView),
+  path("", nippoListView, name="nippo-list"),
+  path("detail/<int:number>/", nippoDetailView, name="nippo-detail"),
+  path("create/", nippoCreateView, name="nippo-create"),
 ]
