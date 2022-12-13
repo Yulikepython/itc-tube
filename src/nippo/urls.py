0c0a1515f671a13a0c9e56e52e4f775586eb63a7
Yu <admin@itc.tokyo>
2022-12-13 13:24:50 +0900

    チュートリアル3-1：データベース内のレコードを取得｜objects.allとobjects.get

--- src/nippo/urls.py
+++ src/nippo/urls.py
@@ -3,6 +3,6 @@ from .views import nippoListView, nippoDetailView,nippoCreateView
  
 urlpatterns = [
   path("", nippoListView, name="nippo-list"),
-  path("detail/<int:number>/", nippoDetailView, name="nippo-detail"),
+  path("detail/<int:pk>/", nippoDetailView, name="nippo-detail"),
   path("create/", nippoCreateView, name="nippo-create"),
 ]
--- src/nippo/views.py
+++ src/nippo/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render
+from .models import NippoModel
+
 from random import randint
 
 def nippoListView(request):
-	return render(request, "nippo/nippo-list.html")
+    template_name = "nippo/nippo-list.html"
+    ctx = {}
+    qs = NippoModel.objects.all()
+    ctx["object_list"] = qs
+    return render(request, template_name, ctx)
 
-
-def nippoDetailView(request, number):
-    template_name="nippo/nippo-detail.html"
-    random_int = randint(1,10)
-    ctx = {
-        "random_number": random_int,
-        "number": number,
-    }
+def nippoDetailView(request, pk):
+    template_name = "nippo/nippo-detail.html"
+    ctx = {}
+    q = NippoModel.objects.get(pk=pk)
+    ctx["object"] = q
     return render(request, template_name, ctx)
 
 def nippoCreateView(request):
