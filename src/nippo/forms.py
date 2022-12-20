91595d79e84df035b5574a1eb3446161d617722d
Yu <admin@itc.tokyo>
2022-12-20 14:53:02 +0900

    チュートリアル4-6：フォームクラスの__init__関数で、Bootstrapクラスを一括で適用する方法

--- src/nippo/forms.py
+++ src/nippo/forms.py
@@ -3,3 +3,8 @@ from django import forms
 class NippoFormClass(forms.Form):
     title = forms.CharField(label="タイトル", widget=forms.TextInput(attrs={'placeholder':'タイトル...'}))
     content = forms.CharField(label="内容", widget=forms.Textarea(attrs={'placeholder':'内容...'}))
+    
+    def __init__(self, *args, **kwargs):
+        for field in self.base_fields.values():
+            field.widget.attrs.update({"class":"form-control"})
+        super().__init__(*args, **kwargs)
