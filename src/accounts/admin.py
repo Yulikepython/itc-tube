9c6b54e5e5b473d2a372a4e81507adf6c469f29a
Yu <admin@itc.tokyo>
2022-12-23 14:09:07 +0900

    チュートリアル6-2：UserAdminでユーザーモデルの管理画面をカスタマイズ

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,5 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User
 
-admin.site.register(User)
+class UserAdmin(BaseUserAdmin):
+    #一覧ページ用
+    list_display = (
+        "email",
+        "active",
+        "staff",
+        "admin",
+    )
+    list_filter = (
+        "admin",
+        "active",
+    )
+    ordering = ("email",)
+    filter_horizontal = ()
+    
+    search_fields = ('email',)
+    
+    #新規登録用
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+            }
+        ),
+    )
+    
+    #編集ページ
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('権限', {'fields': ('staff','admin',)}),
+    )
+
+admin.site.register(User, UserAdmin)
